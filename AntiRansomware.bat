49f2683ecc67f7736af762ab1f75fcb9f390bd53
Joey665 <90168476+Joey665@users.noreply.github.com>
2022-11-30 12:00:19 +0100

    Add files via upload

---
+++ AntiRansomware.bat
@@ -0,0 +1,20 @@
+@echo off
+title AntiRansomware
+echo AntiRansomware
+echo created by Joseph bright
+:start
+echo enter your file name
+set /p name=
+echo the name you have entered of file is %name%
+if EXIST %name% goto infected
+if NOT EXIST %name% goto clean
+cd D:
+:infected
+echo WARNING RANSOMWARE DETECTED !!!
+DEL %name%
+pause
+goto start
+:clean
+echo Sytem Secure!!!
+pause
+exit
